04a6d8edee00f0e34747cbaaef57fea807a655cb
蒋金秋 <978472595@qq.com>
2021-01-29 23:14:28 +0800

    角色控制器基本完成
    视觉和血量显示还未开始。完成了静止和跑动动画。

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,21 +6,92 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
+    public AudioSource audioBGM;
+    public AudioSource actionAudioSource;
+    public AudioClip bgm1;
+    public AudioClip bgm2;
+    [SerializeField] private AudioClip jumpAudio;
+    [SerializeField] private AudioClip knifeAudio;
+    [SerializeField] private AudioClip bulletAudio;
+    [SerializeField] private AudioClip hurtAudio;
+    [SerializeField] private AudioClip powerUpAudio;
+    [SerializeField] private AudioClip collectionAudio;
+
+    private bool _audioEnable = true;
 
     private void Awake()
     {
         instance = this;
     }
 
-    // Start is called before the first frame update
-    private void Start()
+
+    public void ForbiddenAudio()
     {
-        
+        _audioEnable = false;
+        audioBGM.clip = bgm2;
+        audioBGM.loop = true;
+        audioBGM.Play();
     }
 
-    // Update is called once per frame
-    private void Update()
+    public void ResetAudio()
     {
-        
+        _audioEnable = true;
+        audioBGM.clip = bgm1;
+        audioBGM.loop = true;
+        audioBGM.Play();
+    }
+
+    public void JumpAudio()
+    {
+        if (_audioEnable)
+        {
+            actionAudioSource.clip = jumpAudio;
+            actionAudioSource.Play();
+        }
+    }
+
+    public void KnifeAudio()
+    {
+        if (_audioEnable)
+        {
+            actionAudioSource.clip = knifeAudio;
+            actionAudioSource.Play();
+        }
+    }
+
+    public void BulletAudio()
+    {
+        if (_audioEnable)
+        {
+            actionAudioSource.clip = bulletAudio;
+            actionAudioSource.Play();
+        }
+    }
+
+    public void HurtAudio()
+    {
+        if (_audioEnable)
+        {
+            actionAudioSource.clip = hurtAudio;
+            actionAudioSource.Play();
+        }
+    }
+
+    public void PowerUpAudio()
+    {
+        if (_audioEnable)
+        {
+            actionAudioSource.clip = powerUpAudio;
+            actionAudioSource.Play();
+        }
+    }
+
+    public void CollectionAudio()
+    {
+        if (_audioEnable)
+        {
+            actionAudioSource.clip = collectionAudio;
+            actionAudioSource.Play();
+        }
     }
 }
